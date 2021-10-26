3a93d7f867540042af42b76b0326378d36848f13
Jason Wang <jason45992@gmail.com>
2021-10-26 23:00:03 +0800

    clean up code

--- ccaexplorer/lib/main.dart
+++ ccaexplorer/lib/main.dart
@@ -1,57 +1,15 @@
 import 'package:ccaexplorer/home_page.dart';
-import 'package:ccaexplorer/me/setting.dart';
-import 'package:ccaexplorer/login/signup.dart';
-
-import 'package:ccaexplorer/admin/club_management.dart';
-import 'package:ccaexplorer/admin/select_club.dart';
-import 'package:ccaexplorer/home_event_list/event_home_screen.dart';
 import 'package:ccaexplorer/home_event_list/models/event_data_model.dart';
-import 'package:ccaexplorer/me/me_home.dart';
-
-import 'package:ccaexplorer/club/club_detail/club_detail.dart';
-import 'package:ccaexplorer/club/club_detail/club_detail_data.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'dart:async';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:ccaexplorer/app_theme.dart';
-import 'package:ccaexplorer/register.dart';
-//import 'src/event_details/event_detail.dart';
-import 'package:ccaexplorer/home_event_list/event_home_screen.dart';
-import 'login/login.dart';
-import 'event_details/event_detail.dart';
-import 'login/login.dart';
-// import 'src/event_details/event_detail.dart';
-import 'club/club_join/club_join_form.dart';
-import 'src/login/login.dart';
-
-import 'package:ccaexplorer/club/club_list/club_list.dart';
-import 'src/authentication_state.dart'; // new
+import 'src/authentication_state.dart';
 import 'src/widgets.dart';
 import 'authentication.dart';
 import 'home_event_list/models/user_data_model.dart';
-import 'me/admin_menu.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'common_method/common_method_authentication.dart';
 import 'guest_book.dart';
-import 'me/favourates.dart';
-import 'me/edit_profile.dart';
-import 'me/member_list.dart';
-import 'me/edit_profile_test.dart';
-import 'admin/GridViewDemo.dart';
-import 'me/setting.dart';
-import 'admin_image_upload/event_detail_admin.dart';
-import 'admin/GridViewDemo.dart';
-import 'admin/published_events.dart';
-import 'admin/registration_list.dart';
-import 'admin/participant_profile.dart';
-import 'admin/ClubProfileEdit/clubprofile.dart';
-import 'me/me_home.dart';
-import 'login/admin_signup.dart';
-import 'admin/edit_member.dart';
-import 'admin/club_recruitment_department.dart';
-import 'introduction_animation/introduction_animation_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -84,18 +42,8 @@ class App extends StatelessWidget {
         ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      // home: GridViewDemo(title: 'Grid View Demo'),
-      // home: AdminClubManagement(),
-      // home: AdminEventForm(),
-      // home: ClubRecruitmentDepartment(),
-      // home: SignIn(),
-      // home: ClubHomeScreen(),
-      // home: Setting(),
-      // home: PersonalProfile(),
       // home: IntroductionAnimationScreen(),
       home: HomePage(),
-      // home: ClubDetailPage(Club.generateClubs()[0]),
-      // home: MeHome(),
     );
   }
 }
@@ -108,80 +56,6 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('CCA Explorer'),
-      ),
-      body: ListView(
-        children: <Widget>[
-          Image.asset('assets/images/codelab.png'),
-          const SizedBox(height: 8),
-          const IconAndDetail(Icons.calendar_today, 'Title'),
-          const IconAndDetail(Icons.location_city, 'Place'),
-          Consumer<ApplicationState>(
-            builder: (context, appState, _) => Authentication(
-              email: appState.email,
-              loginState: appState.loginState,
-              startLoginFlow: appState.startLoginFlow,
-              verifyEmail: appState.verifyEmail,
-              signInWithEmailAndPassword: appState.signInWithEmailAndPassword,
-              cancelRegistration: appState.cancelRegistration,
-              registerAccount: appState.registerAccount,
-              signOut: appState.signOut,
-            ),
-          ),
-          const Divider(
-            height: 8,
-            thickness: 1,
-            indent: 8,
-            endIndent: 8,
-            color: Colors.grey,
-          ),
-          const Header("What we'll be doing"),
-          const Paragraph(
-            'Join us for a day full of Firebase Workshops and Pizza!',
-          ),
-          // Modify from here
-          Consumer<ApplicationGuestBookState>(
-            builder: (context, appState, _) => Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                if (AuthenticationCommon().loginState ==
-                    ApplicationLoginState.loggedIn) ...[
-                  Header('Discussion'),
-                  GuestBook(
-                    addMessage: (String message) =>
-                        appState.addMessageToGuestBook(message),
-                    messages: appState.guestBookMessages,
-                  ),
-                ],
-              ],
-            ),
-          ),
-          Consumer<ApplicationUserDetailState>(
-            builder: (context, appState, _) => Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                UserDetail(
-                    addUser: (String message) => appState.addUser(message),
-                    userDetails: appState.userDetailList)
-              ],
-            ),
-          ),
-          Consumer<ApplicationEventDetailState>(
-            builder: (context, appState, _) => Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Header(AuthenticationCommon().loginState.toString()),
-                if (AuthenticationCommon().loginState ==
-                    ApplicationLoginState.loggedIn) ...[
-                  Header('Event'),
-                  EventListDetail(
-                      addevent: (String message) => appState.addEvent(message),
-                      eventDetails: appState.eventDetailList)
-                ],
-              ],
-            ),
-          ),
-          // To here.
-        ],
       ),
     );
   }
